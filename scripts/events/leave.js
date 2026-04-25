c09ebe8d52dd546ee1c758ee9e18054c1cfa7cf5
tabassumtanha127t-dot <tabassumtanha.127t@gmail.com>
2026-04-25 09:31:08 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -1,98 +1,49 @@
-const { getTime, drive } = global.utils;
+const { getTime, getStreamFromURL } = global.utils;
 
 module.exports = {
-	config: {
-		name: "leave",
-		version: "1.4",
-		author: "NTKhang",
-		category: "events"
-	},
+    config: {
+        name: "leave",
+        version: "3.0",
+        author: "SAIF",
+        category: "events"
+    },
 
-	langs: {
-		vi: {
-			session1: "sáng",
-			session2: "trưa",
-			session3: "chiều",
-			session4: "tối",
-			leaveType1: "tự rời",
-			leaveType2: "bị kick",
-			defaultLeaveMessage: "{userName} đã {type} khỏi nhóm"
-		},
-		en: {
-			session1: "morning",
-			session2: "noon",
-			session3: "afternoon",
-			session4: "evening",
-			leaveType1: "left",
-			leaveType2: "was kicked from",
-			defaultLeaveMessage: "{userName} {type} the group"
-		}
-	},
+    onStart: async ({ threadsData, message, event, api, usersData }) => {
+        if (event.logMessageType !== "log:unsubscribe") return;
 
-	onStart: async ({ threadsData, message, event, api, usersData, getLang }) => {
-		if (event.logMessageType == "log:unsubscribe")
-			return async function () {
-				const { threadID } = event;
-				const threadData = await threadsData.get(threadID);
-				if (!threadData.settings.sendLeaveMessage)
-					return;
-				const { leftParticipantFbId } = event.logMessageData;
-				if (leftParticipantFbId == api.getCurrentUserID())
-					return;
-				const hours = getTime("HH");
+        const { threadID } = event;
+        const threadData = await threadsData.get(threadID);
 
-				const threadName = threadData.threadName;
-				const userName = await usersData.getName(leftParticipantFbId);
+        if (!threadData.settings.sendLeaveMessage) return;
 
-				// {userName}   : name of the user who left the group
-				// {type}       : type of the message (leave)
-				// {boxName}    : name of the box
-				// {threadName} : name of the box
-				// {time}       : time
-				// {session}    : session
+        const { leftParticipantFbId } = event.logMessageData;
+        if (leftParticipantFbId == api.getCurrentUserID()) return;
 
-				let { leaveMessage = getLang("defaultLeaveMessage") } = threadData.data;
-				const form = {
-					mentions: leaveMessage.match(/\{userNameTag\}/g) ? [{
-						tag: userName,
-						id: leftParticipantFbId
-					}] : null
-				};
+        const userName = await usersData.getName(leftParticipantFbId) || "Someone";
+        const isKicked = leftParticipantFbId != event.author;
 
-				leaveMessage = leaveMessage
-					.replace(/\{userName\}|\{userNameTag\}/g, userName)
-					.replace(/\{type\}/g, leftParticipantFbId == event.author ? getLang("leaveType1") : getLang("leaveType2"))
-					.replace(/\{threadName\}|\{boxName\}/g, threadName)
-					.replace(/\{time\}/g, hours)
-					.replace(/\{session\}/g, hours <= 10 ?
-						getLang("session1") :
-						hours <= 12 ?
-							getLang("session2") :
-							hours <= 18 ?
-								getLang("session3") :
-								getLang("session4")
-					);
+        let leaveMessage;
 
-				form.body = leaveMessage;
+        if (isKicked) {
+            const kickerName = await usersData.getName(event.author) || "Admin";
+            leaveMessage = `${userName} was kicked by ${kickerName}.`;
+        } else {
+            leaveMessage = `${userName} left from the group.`;
+        }
 
-				if (leaveMessage.includes("{userNameTag}")) {
-					form.mentions = [{
-						id: leftParticipantFbId,
-						tag: userName
-					}];
-				}
+        const form = { body: leaveMessage };
 
-				if (threadData.data.leaveAttachment) {
-					const files = threadData.data.leaveAttachment;
-					const attachments = files.reduce((acc, file) => {
-						acc.push(drive.getFile(file, "stream"));
-						return acc;
-					}, []);
-					form.attachment = (await Promise.allSettled(attachments))
-						.filter(({ status }) => status == "fulfilled")
-						.map(({ value }) => value);
-				}
-				message.send(form);
-			};
-	}
+        try {
+            const gifUrl = isKicked
+                ? "https://files.catbox.moe/iddtvw.gif"
+                : "https://files.catbox.moe/fypke2.gif";
+
+            const attachment = await getStreamFromURL(gifUrl);
+            if (attachment) form.attachment = [attachment];
+        } catch (err) {
+            console.error("Gif error:", err);
+        }
+
+        return message.send(form);
+    }
 };
